cc4e0b026073860ea4419038dbf1745bcca84e22
Tit "Titus" Lavrov <titus.mailbox@gmail.com>
2022-02-18 13:45:38 +0100

    TexTools popup restored
    bpy.ops.ui.textools_popup - restored

--- utilities_ui.py
+++ utilities_ui.py
@@ -1,14 +1,12 @@
 import bpy
-import bpy.utils.previews
 import os
-from bpy.types import Panel, EnumProperty, WindowManager
-from bpy.props import StringProperty
 
-from . import settings
 from . import utilities_bake
 from . import op_bake
+from .t3dn_bip import previews
 
-preview_collections = {}
+from bpy.props import StringProperty
+
 
 size_textures = [
 		('32', '32', ''), 
@@ -22,11 +20,35 @@ size_textures = [
 		('8192', '8192', '')
 	]
 
+# Preview collections created in the register function.
+preview_icons = previews.new(max_size=(32, 32))
+thumbnail_previews = None
 
-preview_icons = bpy.utils.previews.new()
+
+def icon_register(fileName):
+	name = fileName.split('.')[0]	# Don't include file extension
+	icons_location = os.path.join(os.path.dirname(__file__), "icons_bip")
+	preview_icons.load_safe(name, os.path.join(icons_location, fileName), 'IMAGE')
+
 
 def icon_get(name):
 	return preview_icons[name].icon_id
+
+
+def generate_bake_mode_previews():
+	image_location = os.path.join(os.path.dirname(__file__), "resources/bake_modes_bip")	
+	enum_items = []
+	
+	# Generate the thumbnails
+	for i, image in enumerate(os.listdir(image_location)):
+		mode = image[0:-4]
+		if mode in op_bake.modes:
+			filepath = os.path.join(image_location, image)
+			thumb = thumbnail_previews.load_safe(filepath, filepath, 'IMAGE')
+			enum_items.append((image, mode, "", thumb.icon_id, i))
+	
+	return enum_items
+
 
 
 def GetContextView3D():
@@ -39,6 +61,7 @@ def GetContextView3D():
 						override = {'window': window, 'screen': screen, 'area': area, 'region': region, 'scene': bpy.context.scene, 'edit_object': bpy.context.edit_object, 'active_object': bpy.context.active_object, 'selected_objects': bpy.context.selected_objects}   # Stuff the override context with very common requests by operators.  MORE COULD BE NEEDED!
 						return override					
 	return None
+
 
 
 def GetContextViewUV():
@@ -54,44 +77,14 @@ def GetContextViewUV():
 
 
 
-
-def icon_register(fileName):
-	name = fileName.split('.')[0]   # Don't include file extension
-	icons_dir = os.path.join(os.path.dirname(__file__), "icons")
-	preview_icons.load(name, os.path.join(icons_dir, fileName), 'IMAGE')
-
-
-
 def get_padding():
 	size_min = min(bpy.context.scene.texToolsSettings.size[0],bpy.context.scene.texToolsSettings.size[1])
 	return bpy.context.scene.texToolsSettings.padding / size_min
 
 
 
-def generate_bake_mode_previews():
-	# We are accessing all of the information that we generated in the register function below
-	preview_collection = preview_collections["thumbnail_previews"]
-	image_location = preview_collection.images_location
-	VALID_EXTENSIONS = ('.png', '.jpg', '.jpeg')
-	
-	enum_items = []
-	
-	# Generate the thumbnails
-	for i, image in enumerate(os.listdir(image_location)):
-		mode = image[0:-4]
-		print(".. .{}".format(mode))
-
-
-		if image.endswith(VALID_EXTENSIONS) and mode in op_bake.modes:
-			filepath = os.path.join(image_location, image)
-			thumb = preview_collection.load(filepath, filepath, 'IMAGE')
-			enum_items.append((image, mode, "", thumb.icon_id, i))
-			
-	return enum_items
-	
-
 def get_bake_mode():
-	return str(bpy.context.scene.TT_bake_mode).replace(".png","").lower()
+	return str(bpy.context.scene.TT_bake_mode).replace(".bip","").lower()
 
 
 class op_popup(bpy.types.Operator):
@@ -99,73 +92,63 @@ class op_popup(bpy.types.Operator):
 	bl_label = "Message"
 
 	message : StringProperty()
- 
+
 	def execute(self, context):
 		self.report({'INFO'}, self.message)
 		print(self.message)
 		return {'FINISHED'}
- 
+
 	def invoke(self, context, event):
 		wm = context.window_manager
 		return wm.invoke_popup(self, width=200)
- 
+
 	def draw(self, context):
 		self.layout.label(text=self.message)
 
 
+def set_bake_color_space_int(bake_mode):
+	preferences = bpy.context.preferences.addons[__package__].preferences
+	if "normal_" in bake_mode:
+		return 1
+	elif preferences.bake_color_space_def == 'STANDARD':
+		return 0
+	elif preferences.bake_color_space_def == 'PBR':
+		if op_bake.modes[bake_mode].material != "" or (bake_mode =='transmission' and not preferences.bool_clean_transmission) or bake_mode in {'diffuse','base_color','sss_color','emission','environment','combined'}:
+			return 0
+		return 1
+
 
 
 def on_bakemode_set(self, context):
-	print("Set  '{}'".format(bpy.context.scene.TT_bake_mode))
-	utilities_bake.on_select_bake_mode(get_bake_mode())
+	bake_mode = get_bake_mode()
+	if set_bake_color_space_int(bake_mode):
+		bpy.context.scene.texToolsSettings.bake_color_space = 'Non-Color'
+	else:
+		bpy.context.scene.texToolsSettings.bake_color_space = 'sRGB'
+	utilities_bake.on_select_bake_mode(bake_mode)
 
 
 
 def register():
-	from bpy.types import Scene
-	from bpy.props import StringProperty, EnumProperty
-	
-	print("_______REgister previews")
+	global thumbnail_previews
+	thumbnail_previews = previews.new(max_size=(128, 128))
 
-	# Operators
-	# bpy.utils.register_class(op_popup)
-
-	# global preview_icons
-	# preview_icons = bpy.utils.previews.new()
-
-	# Create a new preview collection (only upon register)
-	preview_collection = bpy.utils.previews.new()
-	preview_collection.images_location = os.path.join(os.path.dirname(__file__), "resources/bake_modes")
-	preview_collections["thumbnail_previews"] = preview_collection
-
-	
 	# This is an EnumProperty to hold all of the images
 	# You really can save it anywhere in bpy.types.*  Just make sure the location makes sense
-	bpy.types.Scene.TT_bake_mode = EnumProperty(
+	bpy.types.Scene.TT_bake_mode = bpy.props.EnumProperty(
 		items=generate_bake_mode_previews(),
 		update = on_bakemode_set,
-		default = 'normal_tangent.png'
+		default = 'normal_tangent.bip'
 	)
 
-	
+
 def unregister():
-
-	print("_______UNregister previews")
-
-	from bpy.types import WindowManager
-	for preview_collection in preview_collections.values():
-		bpy.utils.previews.remove(preview_collection)
-		preview_collection.clear()
-	
-
-	# Unregister icons
-	# global preview_icons
-	# bpy.utils.previews.remove(preview_icons)
-	preview_icons.clear()
-
+	previews.remove(thumbnail_previews)
+	previews.remove(preview_icons)
 
 	del bpy.types.Scene.TT_bake_mode
-   
+
+
 if __name__ == "__main__":
 	register()
 bpy.utils.register_class(op_popup)
