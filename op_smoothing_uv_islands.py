77cb8961ffc02a22759020d977c67811b6817d83
franMarz <58062362+franMarz@users.noreply.github.com>
2022-03-13 13:33:23 +0100

    Review smoothing_uv_islands

--- op_smoothing_uv_islands.py
+++ op_smoothing_uv_islands.py
@@ -5,6 +5,7 @@ import bmesh
 from . import utilities_uv
 
 
+
 class op(bpy.types.Operator):
 	bl_idname = "uv.textools_smoothing_uv_islands"
 	bl_label = "Sharp edges from Islands"
@@ -42,15 +43,16 @@ def smooth_uv_islands(self, context):
 	bpy.ops.mesh.mark_sharp(clear=True)
 
 	bpy.ops.uv.select_all(action='SELECT')
-	bpy.ops.uv.seams_from_islands(mark_seams=True, mark_sharp=True)
-
-	bm = bmesh.from_edit_mesh(bpy.context.active_object.data)
-	uv_layer = bm.loops.layers.uv.verify()
+	bpy.ops.uv.seams_from_islands(mark_seams=False, mark_sharp=True)
 
 	# Do not create sharp edges if the uv island has a uv seam to itself.
 	# Best example is the lateral surface of a cylinder - which doesn't need 
 	# a sharp edge when unrolled for normal map baking.
+
 	if self.soft_self_border:
+		bm = bmesh.from_edit_mesh(bpy.context.active_object.data)
+		uv_layer = bm.loops.layers.uv.verify()
+
 		islands = utilities_uv.getAllIslands(bm, uv_layer)
 		bpy.ops.uv.select_all(action='SELECT')
 		tested_edges = set()
