ad3be9610222275f82c76081ed679e12760aeba4
Oxicid <Oxicid2019@gmail.com>
2024-03-08 17:59:05 +0300

    Speed up x50 times and using new functions

--- op_island_centralize.py
+++ op_island_centralize.py
@@ -2,8 +2,8 @@ import bpy
 import bmesh
 
 from . import utilities_uv
-
-
+from .utilities_bbox import BBox
+from mathutils import Vector
 
 class op(bpy.types.Operator):
 	bl_idname = "uv.textools_island_centralize"
@@ -13,44 +13,44 @@ class op(bpy.types.Operator):
 	
 	@classmethod
 	def poll(cls, context):
-		if bpy.context.area.ui_type != 'UV':
-			return False
 		if not bpy.context.active_object:
 			return False
 		if bpy.context.active_object.mode != 'EDIT':
 			return False
-		if not bpy.context.object.data.uv_layers:
-			return False
-		if bpy.context.scene.tool_settings.use_uv_select_sync:
-			return False
 		return True
 
-
 	def execute(self, context):
-		udim_tile, column, row = utilities_uv.get_UDIM_tile_coords(bpy.context.active_object)
-		utilities_uv.multi_object_loop(centralize, context, udim_tile, column, row)
+		_, column, row = utilities_uv.get_UDIM_tile_coords(bpy.context.active_object)
+		return self.centralize(column, row)
+
+	@staticmethod
+	def centralize(column, row):
+		selected_objs = utilities_uv.selected_unique_objects_in_mode_with_uv()
+		update_obj = []
+		for obj in selected_objs:
+			bm = bmesh.from_edit_mesh(obj.data)
+			uv_layer = bm.loops.layers.uv.verify()
+			islands = utilities_uv.get_selected_islands(bm, uv_layer, selected=False, extend_selection_to_islands=True)
+
+			if not islands:
+				continue
+
+			changed = False
+			for island in islands:
+				center = BBox.calc_bbox_uv(island, uv_layer).center
+				delta = Vector((round(-center.x + 0.5) + column, round(-center.y + 0.5) + row))
+				if delta != Vector((0, 0)):
+					changed = True
+					utilities_uv.translate_island(island, uv_layer, delta)
+			if changed:
+				update_obj.append(obj)
+				
+		if not update_obj:
+			return {'CANCELLED'}
+
+		for obj in update_obj:
+			bmesh.update_edit_mesh(obj.data)
 		return {'FINISHED'}
-
-
-
-def centralize(context, udim_tile, column, row):
-	selection_mode = bpy.context.scene.tool_settings.uv_select_mode
-	bm = bmesh.from_edit_mesh(bpy.context.active_object.data)
-	uv_layers = bm.loops.layers.uv.verify()
-
-	islands = utilities_uv.getSelectionIslands(bm, uv_layers, extend_selection_to_islands=True)
-
-	for island in islands:
-		island_loops = {loop for face in island for loop in face.loops}
-		boundary_loops = {loop for loop in island_loops if loop.link_loop_radial_next not in island_loops or loop.edge.is_boundary}
-		bounds = utilities_uv.get_BBOX(boundary_loops, bm, uv_layers, are_loops=True)
-		center = bounds['center']
-
-		utilities_uv.move_island(island, round(-center.x + 0.5) + column, round(-center.y + 0.5) + row)
-
-	# Workaround for selection not flushing properly from loops to EDGE Selection Mode, apparently since UV edge selection support was added to the UV space
-	bpy.ops.uv.select_mode(type='VERTEX')
-	bpy.context.scene.tool_settings.uv_select_mode = selection_mode
 
 
 bpy.utils.register_class(op)
