0f948feea8469adc63d7e62759ef20e249c98575
franMarz <franmarzcg@gmail.com>
2024-05-17 14:13:06 +0200

    Tune op_smoothing_uv_islands after pull merge
    Use our default way of checking the Blender version

--- op_smoothing_uv_islands.py
+++ op_smoothing_uv_islands.py
@@ -3,6 +3,7 @@ import math
 import bmesh
 
 from . import utilities_uv
+from . import settings
 
 
 
@@ -75,8 +76,7 @@ def smooth_uv_islands(self, context):
 					tested_edges.add(edge)
 
 	bpy.ops.mesh.customdata_custom_splitnormals_clear()
-	
-	if (bpy.app.version < (4,1,0)):
+	if settings.bversion < 4.1:
 		bpy.context.object.data.use_auto_smooth = True
 		bpy.context.object.data.auto_smooth_angle = math.pi
 
